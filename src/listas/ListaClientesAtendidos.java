4f5e6e9593bb769b1a65c2639c7d9f37cae1fc9e
Hugo Martinez <hugo.kio40@gmail.com>
2022-02-20 11:52:36 -0600

    Se suben los manuales y se realizo una ultima modificacion en la grafica de clientes atendidos

--- src/listas/ListaClientesAtendidos.java
+++ src/listas/ListaClientesAtendidos.java
@@ -76,15 +76,18 @@ public class ListaClientesAtendidos {
         NodoSimple actual= this.primero;
         while( actual!= null){
             Cliente clienteActual = (Cliente) actual.getData();
-            String terminoProceso = clienteActual.isTerminoImpresion() ? "Si" : "No";
-            nombresNodos += "nodo" + actual.hashCode() + "[label=\" ID:" +  clienteActual.getIdCliente() + "\\n" + 
-            "Cliente: " +  clienteActual.getNombre() + "\\n" +  "IMG COLOR: " +  clienteActual.getCantidadColor() + 
-            "\\n" +  "IMG BW: " +  clienteActual.getCantidadBW() + 
-            "\\n" +  "PASOS TOTAL: " +  clienteActual.getTotalPasos() + 
-            "\\n" +  "VENT. ATENCION: " +  clienteActual.getVentanillaAtencion() + 
-            "\\n" +  "TERMINO PROCESO: " +  terminoProceso + "\"]" + "\n";
-            if (actual.getSiguiente() != null)            
-                conexiones += String.format("nodo%d -> nodo%d;\n", actual.hashCode(), actual.getSiguiente().hashCode());
+            if(clienteActual.isTerminoImpresion()) {
+                String terminoProceso = clienteActual.isTerminoImpresion() ? "Si" : "No";
+                nombresNodos += "nodo" + actual.hashCode() + "[label=\" ID:" +  clienteActual.getIdCliente() + "\\n" + 
+                "Cliente: " +  clienteActual.getNombre() + "\\n" +  "IMG COLOR: " +  clienteActual.getCantidadColor() + 
+                "\\n" +  "IMG BW: " +  clienteActual.getCantidadBW() + 
+                "\\n" +  "PASOS TOTAL: " +  clienteActual.getTotalPasos() + 
+                "\\n" +  "VENT. ATENCION: " +  clienteActual.getVentanillaAtencion() + 
+                "\\n" +  "TERMINO PROCESO: " +  terminoProceso + "\"]" + "\n";
+                Cliente sigCliente = (Cliente) actual.getSiguiente().getData();
+                if (actual.getSiguiente() != null && sigCliente.isTerminoImpresion())            
+                    conexiones += String.format("nodo%d -> nodo%d;\n", actual.hashCode(), actual.getSiguiente().hashCode());
+            }
             actual=actual.getSiguiente();
         }
         
